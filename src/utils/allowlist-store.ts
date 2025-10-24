b087f8bf4cac0de72690183392ad9c25b077574e
Felix Nordén <felixnorden@gmail.com>
2025-10-24 20:32:44 +0100

    fix: faucet

--- src/utils/allowlist-store.ts
+++ src/utils/allowlist-store.ts
@@ -3,74 +3,75 @@ import { Address } from "viem";
 import { getCurrentEpoch } from "./time";
 
 export type WalletData = {
-	lastClaimed?: number;
-	lastMessageReset: number;
-	messageCount: number;
-	faucetEnabled: boolean;
+  lastClaimed?: number;
+  lastMessageReset: number;
+  messageCount: number;
+  faucetEnabled: boolean;
 };
 
 export function getWalletStore() {
-	return getStore({ name: "Wallet" });
+  return getStore({ name: "MainnetWallet" });
 }
 
 export async function getWalletData(walletAddress: Address) {
-	const walletStore = getWalletStore();
+  const walletStore = getWalletStore();
 
-	return (await walletStore.get(walletAddress.toLowerCase(), {
-		type: "json",
-	})) as WalletData | undefined;
+  return (await walletStore.get(walletAddress.toLowerCase(), {
+    type: "json",
+  })) as WalletData | undefined;
 }
 
 export async function setWalletData(walletAddress: Address, data: WalletData) {
-	const walletStore = getWalletStore();
+  const walletStore = getWalletStore();
 
-	return walletStore.setJSON(walletAddress.toLowerCase(), data);
+  return walletStore.setJSON(walletAddress.toLowerCase(), data);
 }
 
 export async function removeWallet(walletAddress: Address) {
-	const walletStore = getWalletStore();
+  const walletStore = getWalletStore();
 
-	return walletStore.delete(walletAddress.toLowerCase());
+  return walletStore.delete(walletAddress.toLowerCase());
 }
 
 export async function addWallets(walletAddresses: Address[], data: WalletData) {
-	const walletStore = getWalletStore();
+  const walletStore = getWalletStore();
 
-	await Promise.all(
-		walletAddresses.map((walletAddress: Address) =>
-			walletStore.setJSON(walletAddress.toLowerCase(), data),
-		),
-	);
+  await Promise.all(
+    walletAddresses.map((walletAddress: Address) =>
+      walletStore.setJSON(walletAddress.toLowerCase(), data),
+    ),
+  );
 }
 
 export async function removeWallets(walletAddresses: Address[]) {
-	const walletStore = getWalletStore();
+  const walletStore = getWalletStore();
 
-	await Promise.all(
-		walletAddresses.map((walletAddress: Address) =>
-			walletStore.delete(walletAddress.toLowerCase()),
-		),
-	);
+  await Promise.all(
+    walletAddresses.map((walletAddress: Address) =>
+      walletStore.delete(walletAddress.toLowerCase()),
+    ),
+  );
 }
 
 export async function getAllowlist() {
-	const walletStore = getWalletStore();
+  const walletStore = getWalletStore();
 
-	return walletStore.list();
+  return walletStore.list();
 }
 
 export async function initializeUser(walletAddress: Address) {
-	const walletData = await getWalletData(walletAddress);
+  const walletData = await getWalletData(walletAddress);
 
-	// Enable faucet by default
-	const data = {
-		lastMessageReset: walletData?.lastMessageReset ?? getCurrentEpoch(),
-		faucetEnabled: walletData?.faucetEnabled ?? true,
-		messageCount: walletData?.messageCount ?? 0,
-		lastClaimed: walletData?.lastClaimed,
-	}
+  // Enable faucet by default
+  const data = {
+    lastMessageReset: walletData?.lastMessageReset ?? getCurrentEpoch(),
+    faucetEnabled: walletData?.faucetEnabled ?? true,
+    messageCount: walletData?.messageCount ?? 0,
+    lastClaimed: walletData?.lastClaimed,
+  };
 
-	await setWalletData(walletAddress, data);
+  await setWalletData(walletAddress, data);
 
-	return data
+  return data;
 }
+
